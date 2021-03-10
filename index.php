4d8671656b4efb68b4063e5ca3727f4af3b3f9c6
Otoniel Guerra <Otonielguerra11@gmail.com>
2021-03-09 21:34:37 -0600

    5 commit

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Origin: *');						
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=utf-8");
 
 function conectar(){
 	$user="uotryowypietcsd1";
